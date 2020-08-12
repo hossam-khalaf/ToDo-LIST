03037f704d7d1876b4f2bdc823412688ffbc4ef6
hossam-khalaf <hossamhassan303089@gmail.com>
2020-08-12 20:41:16 +0200

    Filtering

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.todo-btn');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
 
 todoBtn.addEventListener('click', addTodo);
 
 todoList.addEventListener('click', deleteCheck);
+
+filterOption.addEventListener('click', filterTodo);
 
 // functions
 
@@ -36,7 +39,11 @@ function addTodo(e) {
   todoDiv.appendChild(deletedBtn);
 
   //append to list
-  todoList.appendChild(todoDiv);
+  if (todoInput.value === '') {
+    return;
+  } else {
+    todoList.appendChild(todoDiv);
+  }
 
   // clear todo value after
   todoInput.value = '';
@@ -60,4 +67,29 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle('completed');
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
 }
